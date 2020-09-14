c87afca3ba6264f0c27ad8c853b593dc27e4ee33
Specy <dionea2002@gmail.com>
2020-09-14 17:27:56 +0200

    Aggiunti loghi temporanei, aggiunto supporto PWA, aggunte classi

--- public/js/food_list.js
+++ public/js/food_list.js
@@ -1,18 +1,27 @@
-function toggleSelect(toShow){
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/public/service-worker.js')
+      .then((reg) => {
+        console.log('Service worker registered.', reg);
+      });
+  });
+}
+
+function toggleSelect(toShow) {
   let select = document.getElementsByClassName("selHide")
   Array.from(select).forEach(select => {
     select.style.display = "none"
   })
-  toShow = "select"+toShow.innerHTML
+  toShow = "select" + toShow.innerHTML
   document.getElementById(toShow).style.display = "block"
 }
 
-async function initializeFood(){
+async function initializeFood() {
   let menu = await fetch("/data/menu.json").then(data => data.json())
   let keys = Object.keys(menu)
-  for(let i = 0; i<keys.length; i++){
-    let foodType = document.getElementById("select"+keys[i])
-    for(let j = 0; j<menu[keys[i]].length; j++){
+  for (let i = 0; i < keys.length; i++) {
+    let foodType = document.getElementById("select" + keys[i])
+    for (let j = 0; j < menu[keys[i]].length; j++) {
       let option = document.createElement("option")
       option.value = menu[keys[i]][j].name
       option.innerHTML = menu[keys[i]][j].name
@@ -20,4 +29,27 @@ async function initializeFood(){
     }
   }
 }
+
 initializeFood()
+
+class Ordine {
+  constructor() {
+    this.price = 0
+    this.date = 0,
+      this.order = {
+        pizze: [],
+        panini: [],
+        dolci: []
+      }
+  }
+}
+
+class Cibo {
+  constructor(name, description, quantity, price, id) {
+    this.name = name
+    this.description = description
+    this.quantity = quantity
+    this.price = price
+    this.id = id
+  }
+}
--- public/js/indexScript.js
+++ public/js/indexScript.js
@@ -1,50 +1,8 @@
-let ordine = {
-    price: 0,
-    date: 0,
-    order: {
-        pizze: [{
-            name: "",
-            description: "",
-            quantity: 0,
-            price: 0,
-            id: "Panino_" //non obbligatorio
-        }],
-        panini: [],
-        dolci: []
-    }
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/public/service-worker.js')
+            .then((reg) => {
+                console.log('Service worker registered.', reg);
+            });
+    });
 }
-let esempioOrdine = {
-    price: 4.2,
-    date: Date.now(), //in millisecondi
-    order: {
-        pizze: [{
-                name: "Ventricina",
-                quantity: 2,
-                price: 2,
-                id: 3
-            },
-            {
-                name: "Margherita",
-                quantity: 1,
-                price: 1,
-                id: 2
-            }
-        ],
-        panini: [{
-            name: "Ventricina farcito",
-            quantity: 1,
-            price: 1.2,
-            id: 8
-        }],
-        dolci: []
-    }
-}
-
-
-//---------------------------------------//
-
-async function getDati(){
-    let dati = await fetch("./data/menu.json").then(data => data.json())
-    console.log(dati)
-}
-getDati()
