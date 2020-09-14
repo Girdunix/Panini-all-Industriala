f87c852b875f65a1d9f9c19bce07371da9e694c7
Specy <dionea2002@gmail.com>
2020-09-14 13:11:24 +0200

    Ristrutturato git

--- data/Classi/Alunno.js
+++
@@ -1,9 +0,0 @@
-module.exports = class Alunno {
-    constructor(argomenti){
-        this.id = argomenti[0]
-        this.nome = argomenti[1]
-        this.cognome = argomenti[2]
-        this.classe = argomenti[3]
-        this.nomeCognome = this.nome + " " + this.cognome
-    } 
-}
