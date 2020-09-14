0fe7333951aca12cea7167062b0e0245cccb4e3f
Specy <dionea2002@gmail.com>
2020-09-14 14:13:11 +0200

    Aggiunto json per il menu

--- public/js/indexScript.js
+++ public/js/indexScript.js
@@ -1,42 +1,50 @@
-let textareaAlunni = document.getElementById("textareaAlunni")
-async function getDatiAlunni(){
-    let dati = await fetch("/getDatiAlunni").then(dati => dati.json())
-    textareaAlunni.value = JSON.stringify(dati).split("},").join("},\n\n")
+let ordine = {
+    price: 0,
+    date: 0,
+    order: {
+        pizze: [{
+            name: "",
+            description: "",
+            quantity: 0,
+            price: 0,
+            id: "Panino_" //non obbligatorio
+        }],
+        panini: [],
+        dolci: []
+    }
+}
+let esempioOrdine = {
+    price: 4.2,
+    date: Date.now(), //in millisecondi
+    order: {
+        pizze: [{
+                name: "Ventricina",
+                quantity: 2,
+                price: 2,
+                id: 3
+            },
+            {
+                name: "Margherita",
+                quantity: 1,
+                price: 1,
+                id: 2
+            }
+        ],
+        panini: [{
+            name: "Ventricina farcito",
+            quantity: 1,
+            price: 1.2,
+            id: 8
+        }],
+        dolci: []
+    }
+}
+
+
+//---------------------------------------//
+
+async function getDati(){
+    let dati = await fetch("./data/menu.json").then(data => data.json())
     console.log(dati)
 }
-/*
-Leggera spiegazione su cosa è async/await/.then
-
-è la cosa più difficile da capire in javascript, in pratica non tutto è svolto in maniera immediata. 
-per esempio, se il browser deve prendere un file dal server, deve aspettare che il server risponda, non si sa quanto tempo
-può impiegare, per questo motivo esistono le funzioni asincrone (iniziano con async), esse vengono eseguite in maniera non sincroena
-await invece vuol dire che aspetta che un qualcosa sia finito. 
-
-Tutto questo si chiama PROMISE , qui sotto scrivo qualche esempio
-*/
-
-function esempioPromise(){
-    return new Promise((risolvi, getta) =>{
-        setTimeout(function(){
-            risolvi("Risolta in 1000ms!")
-        },1000)
-    })
-}
-
-async function funzioneAsincrona(){
-    console.log(await esempioPromise())
-}
-function funzioneSincrona(){
-    console.log("sincrona")
-}
-funzioneAsincrona()
-funzioneSincrona()
-
-setTimeout(() =>{
-    console.log("/////////")
-    eseguiSincroni()
-},1500)    
-async function eseguiSincroni(){
-    await funzioneAsincrona()
-    funzioneSincrona()
-}
+getDati()
