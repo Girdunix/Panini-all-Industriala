57179a9c8f920b6df21e18e2f78cd9b8e191648c
Specy <dionea2002@gmail.com>
2020-09-14 16:51:36 +0200

    Aggiunti toggle dei select

--- public/js/food_list.js
+++ public/js/food_list.js
@@ -1,10 +1,23 @@
-function hide(id) {
-  let x = document.getElementById("sel" + id)
-  let num = 3 //il numero di select
-
-  for (let i = 0; i < num; i++) {
-    document.getElementById("sel" + i).style.display = "none"
-  }
-
-  x.style.display = "block"
+function toggleSelect(toShow){
+  let select = document.getElementsByClassName("nomeSelect")
+  Array.from(select).forEach(select => {
+    select.style.display = "none"
+  })
+  toShow = "select"+toShow.innerHTML
+  document.getElementById(toShow).style.display = "block"
 }
+
+async function initializeFood(){
+  let menu = await fetch("/data/menu.json").then(data => data.json())
+  let keys = Object.keys(menu)
+  for(let i = 0; i<keys.length; i++){
+    let foodType = document.getElementById("select"+keys[i])
+    for(let j = 0; j<menu[keys[i]].length; j++){
+      let option = document.createElement("option")
+      option.value = menu[keys[i]][j].name
+      option.innerHTML = menu[keys[i]][j].name
+      foodType.appendChild(option)
+    }
+  }
+}
+initializeFood()
