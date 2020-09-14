0f268670381369bba8e0f4b8fec3481b7f03376d
Specy <dionea2002@gmail.com>
2020-09-14 18:01:28 +0200

    reso PWA installabile

--- public/js/food_list.js
+++ public/js/food_list.js
@@ -2,9 +2,9 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/public/service-worker.js')
       .then((reg) => {
-        console.log('Service worker registered.', reg);
-      });
-  });
+        console.log('Service worker registered.', reg)
+      })
+  })
 }
 
 function toggleSelect(toShow) {
