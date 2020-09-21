d7cc83ccc2987e8a836de6c51f7c592b97927273
Federico <federicosantavicca@gmail.com>
2020-09-21 11:47:06 +0200

    Meccanica di login integrata
    Si

--- public/php/login.php
+++ public/php/login.php
@@ -13,6 +13,35 @@ class response
         $this->message = $message;
     }
 }
-$reponse = new response(false, "In costruzione con PHP!");
-echo json_encode($reponse)
+
+$data = json_decode(file_get_contents('php://input')); //dati che vengono dal client
+$hostname = "localhost"; //creazione connessione al database
+$utente = "root";
+$password = "";
+$nomedatabase = "db";
+$mysql = mysqli_connect($hostname, $utente, $password, $nomedatabase);
+if (!$mysql) {
+    $object = new response(false, "Errore connessione!");
+    echo $object = json_encode($object);
+    exit();
+}
+
+//separazione delle credenziali dall'ordine
+$credentials = $data->credentials;
+//$order = json_encode($data->order);
+
+
+//----------------------------CONTROLLA SE LA PASSWORD E' CORRETTA---------------------------------//
+$correctCredentials = mysqli_query($mysql,"SELECT psw FROM utenti WHERE username = '$credentials->username' AND psw = '$credentials->password'");
+//se non esiste nessuna corrispondenza dove l'username e password sono uguali, ritorna l'errore al client 
+if($correctCredentials->num_rows == 0) {
+    $object = new response(false, "Credenziali Sbagliate");
+    echo $object = json_encode($object);
+    exit();
+}
+else{    
+    $object = new response(true, "Connessione Effettuata");
+    echo $object = json_encode($object);
+}
+
 ?>
